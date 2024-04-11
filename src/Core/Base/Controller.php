339afcef4236bb804edeb95562e74a4a69aabd1f
Fernando Diaz <ing.diaz.gil@gmail.com>
2024-04-10 18:05:50 -0600

    Separacion de Mensajes a clase nueva
    Mensajes en una clase cuya es responsabilidad es enviar los mensajes de Respuesta

--- src/Core/Base/Controller.php
+++ src/Core/Base/Controller.php
@@ -34,51 +34,6 @@ class Controller
         return $this->f3->get('BASE');
     }
 
-    protected function response($key,$object){
-        $this->responseArray[$key]=$object;
-    }
-
-    protected function successResponse($message){
-        $data = array();
-        $data['message']=$message;
-        echo json_encode($data);
-        header('Content-type: application/json');
-        http_response_code(Response::HTTP_OK);
-        exit();
-    }
-
-    protected function responseBody($data){
-        echo json_encode($data);
-        header('Content-type: application/json');
-        http_response_code(Response::HTTP_OK);
-        exit();
-    }
-
-    protected function responseBodyOrNotFoundMessage($data){
-        if(empty($data)){
-            $this->errorResponse("Informacion no encontrada",Response::HTTP_NOT_FOUND);
-        }
-        echo json_encode($data);
-        header('Content-type: application/json');
-        http_response_code(Response::HTTP_OK);
-        exit();
-    }
-
-    protected function errorResponse($messages,$httpStatusCode=Response::HTTP_HTTP_UNPROCESSABLE_ENTITY){
-        $data = array();
-        if(is_array($messages)){
-            $data['messages']=$messages;
-            $data['type']="VALIDATION";
-        }else{
-            $data['message']=$messages;
-            $data['type']="GLOBAL";
-        }
-        echo json_encode($data);
-        header('Content-type: application/json');
-        http_response_code($httpStatusCode);
-        exit();
-    }
-
     protected function requestBody($convertToArray=true){
         $jsonBody=file_get_contents('php://input');
         return json_decode($jsonBody,$convertToArray);
---
+++ src/Core/Base/ResponseMessage.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Core\Base;
+
+class ResponseMessage{
+    
+    public function response($key,$object){
+        $this->responseArray[$key]=$object;
+    }
+
+    public function successResponse($message){
+        $data = array();
+        $data['message']=$message;
+        echo json_encode($data);
+        header('Content-type: application/json');
+        http_response_code(Response::HTTP_OK);
+        exit();
+    }
+
+    public function responseBody($data){
+        echo json_encode($data);
+        header('Content-type: application/json');
+        http_response_code(Response::HTTP_OK);
+        exit();
+    }
+
+    public function responseBodyOrNotFoundMessage($data){
+        if(empty($data)){
+            $this->errorResponse("Informacion no encontrada",Response::HTTP_NOT_FOUND);
+        }
+        echo json_encode($data);
+        header('Content-type: application/json');
+        http_response_code(Response::HTTP_OK);
+        exit();
+    }
+
+    public function errorResponse($messages,$httpStatusCode=Response::HTTP_HTTP_UNPROCESSABLE_ENTITY){
+        $data = array();
+        if(is_array($messages)){
+            $data['messages']=$messages;
+            $data['type']="VALIDATION";
+        }else{
+            $data['message']=$messages;
+            $data['type']="GLOBAL";
+        }
+        echo json_encode($data);
+        header('Content-type: application/json');
+        http_response_code($httpStatusCode);
+        exit();
+    }
+
+}
